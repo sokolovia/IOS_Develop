aa49278e976b5d13efbec62dce195d7bdc4010b8
ADAMBAT1912 <adambat@yandex.ru>
2017-10-12 15:33:23 +0300

    Start

---
+++ managerTaskParkMail/managerTaskParkMail/LogInViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  managerTaskParkMail
+//
+//  Created by adam musallam on 12.10.17.
+//  Copyright © 2017 Alphabet. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
